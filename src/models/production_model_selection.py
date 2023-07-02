d13938ca013b3b086a6ac474fee4a7268c001094
abdellatif ouardi <ouardi.abdellatif@gmail.com>
2023-07-02 16:17:38 +0100

    ss

--- src/models/production_model_selection.py
+++ src/models/production_model_selection.py
@@ -5,9 +5,9 @@ from pprint import pprint
 from train_model import read_params
 from mlflow.tracking import MlflowClient
 
-def log_production_model(config_path):
+def log_production_model(config_path):  # sourcery skip: hoist-statement-from-if
     config = read_params(config_path)
-    mlflow_config = config["mlflow_config"] 
+    mlflow_config = config["mlflow_config"]
     model_name = mlflow_config["registered_model_name"]
     model_dir = config["model_dir"]
     remote_server_uri = mlflow_config["remote_server_uri"]
@@ -16,13 +16,13 @@ def log_production_model(config_path):
     runs = mlflow.search_runs(experiment_ids=1)
     max_accuracy = max(runs["metrics.accuracy"])
     max_accuracy_run_id = list(runs[runs["metrics.accuracy"] == max_accuracy]["run_id"])[0]
-    
+
     client = MlflowClient()
     for mv in client.search_model_versions(f"name='{model_name}'"):
         mv = dict(mv)
 
+        current_version = mv["version"]
         if mv["run_id"] == max_accuracy_run_id:
-            current_version = mv["version"]
             logged_model = mv["source"]
             pprint(mv, indent=4)
             client.transition_model_version_stage(
@@ -31,7 +31,6 @@ def log_production_model(config_path):
                 stage="Production"
             )
         else:
-            current_version = mv["version"]
             client.transition_model_version_stage(
                 name=model_name,
                 version=current_version,
